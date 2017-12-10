9825f8b1147af410c45e843e035c0cf3c5b6dfe8
Sando <metatronishe@gmail.com>
2017-12-10 19:04:42 +0200

    final

---
+++ example.py
@@ -0,0 +1,16 @@
+class PageObject(object):
+
+	host = 'demo6144383.mockable.io'
+
+	members = 'members'
+
+	member = 'member'
+
+	memberid = '1'
+
+	success = 200
+
+	listall = 'team_members'
+
+	listid = 'id'
+
