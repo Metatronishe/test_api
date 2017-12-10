4f501e335eff72e523f1ba0cec78a23f0575269f
Sando <metatronishe@gmail.com>
2017-12-10 12:03:40 +0200

    just a little edit

--- test_api.py
+++ test_api.py
@@ -5,27 +5,24 @@ from page_object import PageObject
 
 class TestApi(unittest.TestCase):
 
-	success = 200
-		
-
     def __init__(self, *a, **kw):
-    	super(TestApi, self).__init__(*a, **kw)
-    	self.host = 'demo6144383.mockable.io'
-    	self.members = 'members'
-    	self.member = 'member'
-    	self.id = '1'
-    	self.urlmembers = 'https://{}/{}'.format(self.host, self.members)
-    	self.urlmember = 'https://{}/{}/{}'.format(self.host, self.member, self.id)
+	    super(TestApi, self).__init__(*a, **kw)
+	    self.host = 'demo6144383.mockable.io'
+	    self.members = 'members'
+	    self.member = 'member'
+	    self.id = '1'
+	    self.urlmembers = 'https://{}/{}'.format(self.host, self.members)
+	    self.urlmember = 'https://{}/{}/{}'.format(self.host, self.member, self.id)
 
     def test_1_member(self):
     	status_code, member = self._get_member()
-    	self.assertEqual(status_code, success)
-    	self.assertEqual(member.get(listid), 1)
+    	self.assertEqual(status_code, 200)
+    	self.assertEqual(member.get('id'), 1)
       
     def test_members(self):
     	status_code, members = self._get_members()
-    	self.assertEqual(status_code, success)
-    	self.assertGreater(len(members.get(listall)), 1)
+    	self.assertEqual(status_code, 200)
+    	self.assertGreater(len(members.get('team_members')), 1)
 
     def _get_members(self):
     	_url = self.urlmembers
